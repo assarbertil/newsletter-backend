8a6c22be7c2c0a29ec8ff3fbbaee9b917d8d25b5
Assar <33634310+assarbertil@users.noreply.github.com>
2022-05-26 16:25:02 +0200

    admin interface done

--- public/script.js
+++ public/script.js
@@ -3,99 +3,76 @@ const passwordField = document.getElementById("password");
 const subscribeField = document.getElementById("subscribe");
 
 // Register a user based on the input parameters
-const registerUser = (email, password, subscribe) =>
-  fetch("/api/user", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
+const registerUser = (email, password, subscribe) => {
+  axios
+    .post("/api/user", {
       email,
       password,
       isSubscribed: subscribe,
-    }),
-  }).then(res => {
-    if (res.status === 201) {
-      res.json().then(user => {
-        console.log(user.isSubscribed);
-        const row = `
-        <tr data-user="${user._id}" class="slide-in">
-          <td>${user.email}</td>
-          <td>
-            <input data-is-subscribed data-id="${
-              user._id
-            }" type="checkbox" name="subscribed-${user._id}" ${
-          user.isSubscribed ? "checked" : ""
-        } />
-          </td>
-          <td>
-            <button data-id="${user._id}" data-email="${
-          user.email
-        }" class="delete-button">Delete</button>
-          </td>
-        <tr>`;
+    })
+    .then(({ data: user }) => {
+      const row = `
+      <tr data-user="${user._id}" class="slide-in">
+        <td>${user.email}</td>
+        <td>
+          <input data-is-subscribed data-id="${
+            user._id
+          }" type="checkbox" name="subscribed-${user._id}" ${
+        user.isSubscribed ? "checked" : ""
+      } />
+        </td>
+        <td>
+          <button data-id="${user._id}" data-email="${
+        user.email
+      }" class="delete-button">Delete</button>
+        </td>
+      <tr>`;
 
-        document.getElementById("users").insertAdjacentHTML("beforeend", row);
-        addDeleteHandlers();
-        addToggleSubscribedHandlers();
+      document.getElementById("users").insertAdjacentHTML("beforeend", row);
+      addDeleteHandlers();
+      addToggleSubscribedHandlers();
 
-        emailField.value = "";
-        passwordField.value = "";
-        subscribeField.checked = true;
-      });
-    } else {
-      res.json().then(data => alert(data.message));
-    }
-  });
+      emailField.value = "";
+      passwordField.value = "";
+      subscribeField.checked = true;
 
-/**
- * Delete user by id
- */
+      setTimeout(row.remove(), 0.3);
+    })
+    .catch(err => alert(err.response.data.message));
+};
+
+// Delete user by id
 const deleteUser = (id, email) => {
   if (confirm(`Are you sure you want to delete ${email}?`)) {
-    fetch(`/api/user/${id}`, {
-      method: "DELETE",
-    }).then(res => {
-      if (res.status === 200) {
+    axios
+      .delete(`/api/user/${id}`)
+      .then(res => {
         const row = document.querySelector(`[data-user="${id}"]`);
         console.log(row);
         row.classList.contains("slide-in") && row.classList.remove("slide-in");
         row.classList.add("slide-out");
-      } else {
-        res.json().then(data => alert(data.message));
-      }
-    });
+      })
+      .catch(err => alert(err.response.data.message));
   }
 };
 
-/**
- * Set subscribed status for user by id
- */
-const setSubscribed = (id, boolean) =>
-  fetch(`/api/user/${id}`, {
-    method: "PATCH",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ isSubscribed: boolean }),
-  }).then(res => {
-    if (res.status === 200) {
-      // location.reload();
-    } else {
-      res.json().then(data => alert(data.message));
-    }
-  });
+// Set subscribed status for user by id
+const setSubscribedStatus = (id, boolean) => {
+  axios
+    .patch(`/api/user/${id}`, { isSubscribed: boolean })
+    .catch(err => alert(err.response.data.message));
+};
 
 const form = document.getElementById("register");
 
-/**
- * Form submit handler, send the data to the server
- */
+// Form submit handler, send the data to the server
 form.onsubmit = e => {
   e.preventDefault();
 
   registerUser(emailField.value, passwordField.value, subscribeField.checked);
 };
 
-/**
- * Iterate over all delete buttons and add a click handler
- */
+// Iterate over all delete buttons and add a click handler
 const addDeleteHandlers = () => {
   document.querySelectorAll(".delete-button").forEach(input => {
     input.onclick = e => {
@@ -104,17 +81,14 @@ const addDeleteHandlers = () => {
   });
 };
 
-/**
- * Iterate over all checkboxes and add a change handler
- */
+// Iterate over all checkboxes and add a change handler
 const addToggleSubscribedHandlers = () => {
   document.querySelectorAll("[data-is-subscribed]").forEach(input => {
-    input.onchange = e => setSubscribed(e.target.dataset.id, e.target.checked);
+    input.onchange = e =>
+      setSubscribedStatus(e.target.dataset.id, e.target.checked);
   });
 };
 
-/**
- * Initially run the handlers
- */
+// Initially add the handlers
 addDeleteHandlers();
 addToggleSubscribedHandlers();
