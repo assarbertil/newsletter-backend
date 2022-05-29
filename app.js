95c5043434670c607f9014e738fbb35eea3ef351
Assar <33634310+assarbertil@users.noreply.github.com>
2022-05-30 00:23:50 +0200

    wrong name for whitelist

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(
   cors({
     credentials: true,
     origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1) {
+      if (corsWhitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
